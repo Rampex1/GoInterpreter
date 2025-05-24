f18b4e02324bce8f2645e9a854d3d3b40961b759
David Zhou <david.zhou.0110@gmail.com>
2025-05-24 02:06:00 -0400

    DEV: Finished parsing

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/Rampex1/GoInterpreter/lexer"
-	"github.com/Rampex1/GoInterpreter/token"
+	"github.com/Rampex1/GoInterpreter/parser"
 	"io"
 )
 
@@ -22,9 +22,42 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		if _, err := io.WriteString(out, program.String()); err != nil {
+			fmt.Fprintf(out, "error writing program output: %v\n", err)
+		}
+		if _, err := io.WriteString(out, "\n"); err != nil {
+			fmt.Fprintf(out, "error writing newline: %v\n", err)
 		}
 	}
 }
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
+const MONKEY_FACE = `            
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
